605f5abae508cd8b284792d960cda9e59ce13357
Nadif Ali Fahim <nadifalifahim@gmail.com>
2024-07-05 20:53:07 +0600

    update v2

--- src/app/(pages)/settings/projects/[projectID]/page.tsx
+++ src/app/(pages)/settings/projects/[projectID]/page.tsx
@@ -1,8 +1,7 @@
-import withAuth from "@/components/Authentication/withAuth";
 import React from "react";
 
 const ProjectDetailsPage = () => {
   return <div>Hello</div>;
 };
 
-export default withAuth(ProjectDetailsPage);
+export default ProjectDetailsPage;
